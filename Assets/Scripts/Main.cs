975c0b0143bbe06b07ebfb3198030af4d5d5eea6
Sarat <saratovcev-1995@mail.ru>
2022-12-10 09:16:28 +0300

    Searization. Part I: Standart tools. Correct

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,37 +2,71 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Security;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using TreeEditor;
 using UnityEngine;
+using static UnityEditor.ShaderData;
 
 public class Main : MonoBehaviour
 {
-    [SerializeField] private int numberOfElement = 5;
+    [SerializeField] private int _numberOfElement = 5;
+    [SerializeField] private string _fileName = "note.dat";
 
+    private ListRand _list = new ListRand();
+
+    private string _path = string.Empty;
 
     void Start()
     {
-        ListRand list = new ListRand();
+        InitializeList();
+
+        _path = Path.Combine(Application.dataPath, _fileName);
+
+        Debug.Log("dataPath : " + _path);
+
+        var formatter = new BinaryFormatter();
+
+        using (FileStream fstream = new FileStream(_path, FileMode.OpenOrCreate))
+        {
+            formatter.Serialize(fstream, _list);
+        }
+
+        var newList = new ListRand();
+
+        using (FileStream fstream = new FileStream(_path, FileMode.OpenOrCreate))
+        {
+            newList = (ListRand)formatter.Deserialize(fstream);
+        }
+
+    }
+
+    private void InitializeList()
+    {
+        _list = new ListRand();
 
         int value = 0;
-        for (int i = 0; i < numberOfElement; i++)
-            list.Add(new ListNode(Convert.ToString(++value)));
+        for (int i = 0; i < _numberOfElement; i++)
+            _list.Add(new ListNode(Convert.ToString(++value)));
 
-        var secondNode = list.GetByIndex(1);
+        var secondNode = _list.GetByIndex(1);
             secondNode.SetRand(secondNode);
 
-        var thirdNode = list.GetByIndex(2);
-            thirdNode.SetRand(list.GetLast());
+        var thirdNode = _list.GetByIndex(2);
+        thirdNode.SetRand(_list.GetLast());
 
-        var fourthNode = list.GetByIndex(3);
-            fourthNode.SetRand(list.GetFirst());
+        var fourthNode = _list.GetByIndex(3);
+        fourthNode.SetRand(_list.GetFirst());
     }
 }
 
+[Serializable]
 class ListNode
 {
     public ListNode Prev;
     public ListNode Next;
-    public ListNode Rand; // произвольный элемент внутри списка
+    public ListNode Rand;
     public string Data;
 
     public ListNode()
@@ -58,6 +92,7 @@ class ListNode
     public void SetRand(ListNode rand) => Rand = rand;
 }
 
+[Serializable]
 class ListRand
 {
     public ListNode Head;
@@ -109,6 +144,7 @@ class ListRand
     }
     #endregion
 
+    #region GetElement
     public ListNode GetByIndex(int index = 0)
     {
         if (index < 0 && index >= Count) return null;
@@ -132,20 +168,22 @@ class ListRand
     public ListNode GetFirst() => Head;
     public ListNode GetLast() => Tail;
 
+    #endregion
+
     //Test
-    public void Show()
-    {
-        var item = Head;
-        for (int i = 0; i < Count; i++)
-        {
-            Debug.Log($"Index = { i }, value = { item?.Data }");
+    //public void Show()
+    //{
+    //    var item = Head;
+    //    for (int i = 0; i < Count; i++)
+    //    {
+    //        Debug.Log($"Index = { i }, value = { item?.Data }");
 
-            if (item.Next == null)
-                break;
+    //        if (item.Next == null)
+    //            break;
 
-            item = item.Next;
-        }
-    }
+    //        item = item.Next;
+    //    }
+    //}
 
     public void Serialize(FileStream s) { }
 
