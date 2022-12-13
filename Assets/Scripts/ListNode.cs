f2cd4e194552747cc9fd49a799eee577d081a232
Sarat <saratovcev-1995@mail.ru>
2022-12-13 21:16:44 +0300

    Searization. Part II: Refactoring.Part III.III
    Transfer classes of list item to their own file.

---
+++ Assets/Scripts/ListNode.cs
@@ -0,0 +1,22 @@
+﻿class ListNode
+{
+    public ListNode Prev;
+    public ListNode Next;
+    public ListNode Rand;
+    public string Data;
+
+    public ListNode()
+    {
+        Prev = null;
+        Next = null;
+        Rand = null;
+        Data = string.Empty;
+    }
+    public ListNode(string value)
+    {
+        Prev = null;
+        Next = null;
+        Rand = null;
+        Data = value;
+    }
+}
