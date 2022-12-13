eeecf1b8081695b361416e43a39b5b300e8b9e0c
Sarat <saratovcev-1995@mail.ru>
2022-12-13 21:13:55 +0300

    Searization. Part II: Refactoring.Part III.II
    Translate to dat format. Little test.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     [SerializeField] private int _numberOfElement = 5;
-    [SerializeField] private string _fileName = "note.txt";
+    [SerializeField] private string _fileName = "note.dat";
 
     private ListRand _list = new ListRand();
 
